b25fb780e737b5d9c7f46b62414014be93eff919
Srinivasan R <babbloo85@gmail.com>
2020-03-18 11:57:55 -0700

    get residual power

--- get_ilc.py
+++ get_ilc.py
@@ -1,5 +1,3 @@
-
-
 import argparse, sys, numpy as np, scipy as sc
 sys.path.append('/Users/sraghunathan/Research/SPTPol/analysis/git/tools/')
 import flatsky, tools, misc
@@ -27,7 +25,6 @@ for kargs in args_keys:
     else:
         cmd = '%s = %s' %(kargs, param_value)
     exec(cmd)
-
 freqarr = np.asarray( freqarr )
 ############################################################################################################
 # read and store param dict
@@ -35,16 +32,18 @@ param_dict = misc.fn_get_param_dict(paramfile)
 el = np.arange(param_dict['lmax'])
 
 ############################################################################################################
-#get beam deconvolved noise nls
-
+#collect beam and noise into a dic
 beam_noise_dic = {}
 for (freq, beam, noise) in zip(freqarr, beamarr, noisearr):
     beam_noise_dic[freq] = [beam, noise]
 
+############################################################################################################
+#collect elknee and alpha into a dic
 elknee_dic = {}
 for (freq, elknee, alphaknee) in zip(freqarr, elkneearr, alphakneearr):
     elknee_dic[freq] = [elknee, alphaknee]
-
+############################################################################################################
+#get beam deconvolved noise nls
 nl_dic = {}
 for freq in freqarr:
     beamval, noiseval = beam_noise_dic[freq]
@@ -52,11 +51,7 @@ for freq in freqarr:
     nl[el<=param_dict['lmin']] = 0.
     nl[nl == 0.] = np.min(nl[nl!=0.])/1e3
     nl_dic[freq] = nl
-    
-print(nl_dic.keys())
-
 ############################################################################################################
-
 #get the CMB, noise, and foreground covriance
 try:
     ignore_fg = param_dict['ignore_fg']
@@ -64,218 +59,27 @@ except:
     ignore_fg = []
 print(ignore_fg)
 el, cl_dic = ilc.get_covariance_dic(param_dict, freqarr, nl_dic = nl_dic, ignore_fg = ignore_fg)
-print(el)
-sys.exit()
 ############################################################################################################
-
-nc = len(freqarr)
-acap = np.zeros(nc) + 1. #assuming CMB is the same and calibrations factors are same for all channels
-acap = np.mat(acap).T #should be nc x 1
-
-def fn_create_Clmat(elcnt):
-	Clmat = np.zeros( (nc, nc) )
-	for ncnt1, nuval1 in enumerate(freqarr):
-		for ncnt2, nuval2 in enumerate(freqarr):
-			#Clmat[ncnt2, ncnt1] = Cls_dic[(nuval1, nuval2)][elcnt,1]
-			Clmat[ncnt2, ncnt1] = Cls_dic[(nuval1, nuval2)][elcnt]
-	return Clmat
-
-CLEANED_POW_SPEC = np.zeros( (len(els)) )
-for elcnt, el in enumerate(els):
-	Clmat = np.mat( fn_create_Clmat(elcnt) )
-	Clinv = sc.linalg.pinv2(Clmat)
-	
-	nr = 1.
-	dr = np.dot( acap.T, np.dot(Clinv, acap) )
-	final_pow = np.asarray(nr/dr).squeeze()
-
-	CLEANED_POW_SPEC[elcnt] = final_pow
-
-#RES_POW_SPEC = (Dls_len[:,1]*1e12) - CLEANED_POW_SPEC
-RES_POW_SPEC = CLEANED_POW_SPEC - Cls_CMB
-
-'''
-#1/f component
-elknee, alpha = 100., 1.
-#RES_POW_SPEC_with_oneoverf = RES_POW_SPEC_with_oneoverf * (1 + (elknee/els)**2.)
-
-l1, l2 = 400, 500
-indsforfitting = np.where((els>=l1) & (els<=l2))
-poly_fit_eq=np.polyfit(els[indsforfitting],RES_POW_SPEC[indsforfitting],3)
-poly_fit=np.poly1d(poly_fit_eq)
-indsforfitting = np.where((els<=l1))
-RES_POW_SPEC[indsforfitting]=poly_fit(els[indsforfitting])
-
-RES_POW_SPEC_with_oneoverf = np.copy(RES_POW_SPEC)
-RES_POW_SPEC_with_oneoverf[:] = np.median(RES_POW_SPEC_with_oneoverf)
-RES_POW_SPEC_with_oneoverf = RES_POW_SPEC_with_oneoverf * (1 + (elknee/els)**alpha)
-'''
-
-Dls_fac[:] = 1.
-
-print noisearr, beamarr
-
-if which_exp == 'CMB-HD' and pol:
-	poly_deg = 1
-	poly_fit_eq=np.polyfit(els, RES_POW_SPEC, poly_deg)
-	poly_fit=np.poly1d(poly_fit_eq)
-	RES_POW_SPEC = poly_fit(els)
+#get the residual power now
+cl_residual = ilc.residual_power(param_dict, freqarr, el, cl_dic, final_comp = 'CMB')#, freqcalib_fac = None)
+print(cl_residual)
+############################################################################################################
 
 if 1==1:#show_plots:
-	clf(); ax = subplot(111, xscale = 'log', yscale = 'log')
-	#plot(Dls_len[:,0], Dls_len[:,1] * 1e12 * sims.Tcmb**2., 'k', lw = 3.)
-	plot(els, Cls_CMB  * Dls_fac, 'k', lw = 3.)
-	plot(els, CLEANED_POW_SPEC * Dls_fac, 'gold', label = 'FG cleaned')
-	plot(els, RES_POW_SPEC * Dls_fac, 'lime', label = 'Residual')
-	if year <> -1.:
-		plot(els, RES_POW_SPEC * Dls_fac / yearscaling, 'm', label = 'scaled from full')
-	### plot(els, RES_POW_SPEC_with_oneoverf * Dls_fac, 'lightskyblue', label = 'Residual with 1/f')
-	plot(els, Nl150 * Dls_fac, 'r', label = '150 GHz - White')
-	plot(els, Nl_futuristic * Dls_fac, 'm', label = 'Futuristic - White')
-	legend(loc=1, fancybox=1);xlim(1,12e3);#ylim(1e-3,1e4);#xlim(100,2e4)#;ylim(1,1e4)
-	title('EXperiment = %s' %(which_exp.replace('_','\_')))
-	show()
-end = time.time()
-print 'Time for get final power spectrum = %.1f' %((end-start))
+    from pylab import *
+    freq0 = param_dict['freq0']
+    el_, cl_cmb = fg.get_foreground_power_george_2015('CMB', freq1 = freq0)
+    cl_cmb = cl_cmb[:len(el)]
+    clf(); 
+    ax = subplot(111, xscale = 'log', yscale = 'log')
+    plot(el, cl_cmb, 'k', lw = 3.)
+    plot(el, cl_residual, 'lime', label = 'Residual')
+    plot(el, nl_dic[freq0], 'r', label = '150 GHz - White')
+    legend(loc=1, fancybox=1);
+    #xlim(1,12e3);#ylim(1e-3,1e4);#xlim(100,2e4)#;ylim(1,1e4)
+    show()
 
-import pickle, gzip
-Nl_dic = {}
-Nl_dic['Cl_CMB'] = [els, Cls_CMB]
-Nl_dic['Cl_CMB_cleaned'] = [els, CLEANED_POW_SPEC]
-Nl_dic['Cl_residual'] = [els, RES_POW_SPEC]
-Nl_dic['channels'] = freqarr
-Nl_dic['beams'] = beamarr
-Nl_dic['noiselevels'] = noisearr
-Nl_dic['beam_effective_arcmins'] = op_beam
-Nl_dic['Bl_effective'] = op_Bl
-
-
-
-if not pol:
-	opf = 'data/foreground/cleaning/Cls_ILC_%s.pkl.gz' %(which_exp)
-else:
-	opf = 'data/foreground/cleaning/Cls_ILC_%s_pol.pkl.gz' %(which_exp)
-
-
-if use_lknee:
-	Nl_dic['lknee'] = elkneedic
-	print elkneedic
-
-	opf = opf.replace('.pkl.gz', 'with_1_f_noise.pkl.gz')	
-
-if year<>-1.:
-	opf = opf.replace('.pkl.gz', '_year_%s.pkl.gz' %(year))
-
-if which_exp == 'S4_may_2019':
-	opf = 'data/foreground/cleaning/%s/Cls_ILC.pkl.gz' %(which_exp)
-	opf = opf.replace('.pkl.gz', '_beamx%s_noisex%s.pkl.gz' %(s4_beam_scale_factor, s4_noise_scale_factor))
-
-	print opf
-
-
-pickle.dump(Nl_dic, gzip.open(opf, 'wb'), protocol = 2)
-
-quit()
-
-############################################################################################################
-############################################################################################################
-############################################################################################################
-#step3: get SMICA weights
-"""
-arXiv: 1303.5072 - appendix D; Eq. D2
-"""
-start = time.time()
-nc = len(freqarr)
-acap = np.zeros(nc) + 1. #assuming CMB is the same and calibrations factors are same for all channels
-acap = np.mat(acap).T #should be nc x 1
-
-def fn_create_Clmat(elcnt):
-	Clmat = np.zeros( (nc, nc) )
-	for ncnt1, nuval1 in enumerate(freqarr):
-		for ncnt2, nuval2 in enumerate(freqarr):
-			#Clmat[ncnt2, ncnt1] = Cls_dic[(nuval1, nuval2)][elcnt,1]
-			Clmat[ncnt2, ncnt1] = Cls_dic[(nuval1, nuval2)][elcnt]
-	return Clmat
-
-W = np.zeros( (nc, len(els)) )
-for elcnt, el in enumerate(els):
-	Clmat = np.mat( fn_create_Clmat(elcnt) )
-	Clinv = sc.linalg.pinv2(Clmat)
-	
-	nr = np.dot(Clinv, acap)
-	dr = np.dot( acap.T, np.dot(Clinv, acap) )
-	weight_mat = np.asarray(nr/dr).squeeze()
-
-	W[:, elcnt] = weight_mat
-
-end = time.time()
-print 'Time for get weights = %.1f' %((end-start))
-
-ax = subplot(111)
-for ncnt, nuval in enumerate(freqarr):
-	plot(els, W[ncnt], label = nuval)		
-
-#ylim(-3.,3.)
-legend(loc = 1, fancybox = 1)
-show();quit()
-############################################################################################################
-############################################################################################################
-############################################################################################################
-#step4: get Nl for each channel and the final combination
-maxel = int( max(els) + 1 )
-ax = subplot(111, xscale = 'log', yscale = 'log')
-use_beam_window = 1
-colorarr =['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728','#9467bd', '#8c564b', '#e377c2', '#7f7f7f','#bcbd22', '#17becf']
-Nl_tot = np.zeros( len(els) )
-for ncnt, (nuval, noiseval, beamval) in enumerate(zip(freqarr, noisearr, beamarr)):
-
-	if use_beam_window:
-		fwhm_radians = np.radians(beamval/60.)
-		sigma = fwhm_radians / np.sqrt(8. * np.log(2.))
-		sigma2 = sigma ** 2
-		Bl = np.exp(els * (els+1) * sigma2)
-
-	delta_T_radians = noiseval * np.radians(1./60.)
-	Nl = np.tile(delta_T_radians**2., maxel)
-
-	Nl = np.asarray( [Nl[int(el)] for el in els] )
-
-
-	if use_beam_window: Nl *= Bl
-	Nl_weighted = W[ncnt]**2. * Nl
-
-	#Nl_tot += (1./Nl_weighted)
-	Nl_tot += Nl_weighted
-
-	Dls_fac = els * (els+1) / 2 / np.pi
-	plot(els, Nl * Dls_fac, label = nuval, color = colorarr[ncnt])
-	plot(els, Nl_weighted * Dls_fac, color = colorarr[ncnt], marker = ',', ls = 'None')
-
-#Nl_tot = 1./Nl_tot
-import pickle, gzip
-Nl_dic = {}
-Nl_dic['Nl'] = [els, Nl_tot]
-Nl_dic['channels'] = freqarr
-Nl_dic['beams'] = beamarr
-Nl_dic['noiselevels'] = noisearr
-Nl_dic['beam_effective_arcmins'] = op_beam
-Nl_dic['Bl_effective'] = op_Bl
-
-"""
-if not pol:
-	pickle.dump(Nl_dic, gzip.open('data/foreground/cleaning/Nl_%s.pkl.gz' %(which_exp), 'wb'), protocol = 2)
-else:
-	pickle.dump(Nl_dic, gzip.open('data/foreground/cleaning/Nl_%s_pol_with_extragal_FG.pkl.gz' %(which_exp), 'wb'), protocol = 2)
-"""
-plot(Dls_len[:,0], Dls_len[:,1] * 1e12 * sims.Tcmb**2., 'k')
-plot(els, Nl_tot * Dls_fac, color='darkred', lw = 3.)
-xlim(500,1e4)
-ylim(0.01,1e5)
-title('Experiment = %s' %(which_exp))
-legend(loc = 1, fancybox = 1)
-show()
-
-	
+    
 
 
 
--- ilc.py
+++ ilc.py
@@ -1,6 +1,148 @@
 import numpy as np, sys, os, scipy as sc, healpy as H, foregrounds as fg
 ################################################################################################################
 
+def get_covariance_dic(param_dict, freqarr, nl_dic = None, ignore_fg = []):
+
+    #get the Cls for the baseline band
+    el, cl_cmb = fg.get_foreground_power_george_2015('CMB', freq1 = param_dict['freq0'], freq2 = param_dict['freq0'])
+    el, cl_ksz = fg.get_foreground_power_george_2015('kSZ', freq1 = param_dict['freq0'], freq2 = param_dict['freq0'])
+
+    cl_dic = {}
+
+    for freq1 in freqarr:
+        for freq2 in freqarr:
+
+            if (freq2, freq1) in cl_dic:
+                cl_dic[(freq1, freq2)] = cl_dic[(freq2, freq1)]
+                continue
+
+            #get dust
+            el,  cl_dg_po, cl_dg_clus = fg.get_cl_dust(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'], spec_index_dg_po = param_dict['spec_index_dg_po'], spec_index_dg_clus = param_dict['spec_index_dg_clus'], Tcib = param_dict['Tcib'])
+
+            #get tsz
+            el, cl_tsz = fg.get_cl_tsz(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'])
+
+            #get radio
+            el, cl_radio = fg.get_cl_radio(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'], spec_index_rg = param_dict['spec_index_rg'])
+
+            if 'cmb' not in ignore_fg:
+                cl = np.copy(cl_cmb)
+            if 'ksz' not in ignore_fg:
+                cl = cl + cl_ksz             
+            if 'tsz' not in ignore_fg:
+                cl = cl + cl_tsz
+            if 'radio' not in ignore_fg:
+                cl = cl + cl_radio
+            if 'dust' not in ignore_fg:
+                cl = cl + cl_dg_po + cl_dg_clus
+
+            #make sure cl start from el=0 rather than el=10 which is the default for SPT G15 results
+            lmin = min(el)
+            cl = np.concatenate( (np.zeros(lmin), cl) )
+
+            #noise auto power spectrum
+            if nl_dic is not None:
+
+                nl = nl_dic[freq1]                    
+                if freq1 != freq2: 
+                    nl = np.copy(nl) * 0.
+
+                if len(cl) > len(nl):
+                    cl = cl[:len(nl)]
+                elif len(cl) < len(nl):
+                    nl = nl[:len(cl)]
+
+                el = np.arange(len(cl))
+
+            else:
+                nl = np.zeros(len(cl))
+                #20191116 - fix this: there must be noise correlation in case of atmospheric noise
+                print('\n\n\t\tfix me: there must be noise correlation in case of atmospheric noise')
+
+            cl = cl + nl
+
+            cl[np.isnan(cl)] = 0.
+
+            ##loglog(cl); title('%s - %s' %(freq1, freq2)); show()
+
+            cl_dic[(freq1, freq2)] = cl 
+
+
+    return el, cl_dic  
+
+################################################################################################################
+def create_clmat(freqarr, elcnt, cl_dic):
+    """
+    freqarr  = array of frequency channel
+    elcnt = \el index
+    cl_dic = cl_cmb + cl_FG auto and cross spectra fr thefrequency channel
+    """
+    nc = len(freqarr)
+    clmat = np.zeros( (nc, nc) )
+    for ncnt1, freq1 in enumerate(freqarr):
+        for ncnt2, freq2 in enumerate(freqarr):
+            clmat[ncnt2, ncnt1] = cl_dic[(freq1, freq2)][elcnt]
+    return clmat
+
+################################################################################################################
+def residual_power(param_dict, freqarr, el, cl_dic, final_comp = 'CMB', freqcalib_fac = None):
+
+    acap = get_acap(freqarr, final_comp = final_comp, freqcalib_fac = freqcalib_fac)
+
+    cl_cleaned = np.zeros( (len(el)) )
+    for elcnt, el in enumerate(el):
+        clmat = np.mat( create_clmat(freqarr, elcnt, cl_dic) )
+        clinv = sc.linalg.pinv2(clmat)
+        
+        nr = 1.
+        dr = np.dot( acap.T, np.dot(clinv, acap) )
+        cl_cleaned[elcnt] = np.asarray(nr/dr).squeeze()
+
+    if final_comp == 'CMB':
+        el, cl_ini = fg.get_foreground_power_george_2015('CMB', freq1 = param_dict['freq0'])
+    elif final_comp == 'kSZ':
+        el, cl_ini = fg.get_foreground_power_george_2015('kSZ', freq1 = param_dict['freq0'])
+    elif final_comp == 'tSZ' or final_comp == 'comptony':
+        el, cl_ini = fg.get_foreground_power_george_2015('tSZ', freq1 = param_dict['freq0'])
+        if final_comp == 'comptony':
+            freqscale_fac = compton_y_to_delta_Tcmb(param_dict['freq0'] * 1e9)
+            cl_ini = cl_ini/freqscale_fac
+
+    cl_ini = cl_ini[:len(cl_cleaned)]
+
+    cl_residual = cl_cleaned - cl_ini
+    
+    return cl_residual
+
+################################################################################################################
+
+def get_acap(freqarr, final_comp = 'CMB', freqcalib_fac = None):
+
+    nc = len(freqarr)
+
+    if freqcalib_fac is None: freqcalib_fac = np.ones(nc)
+
+    if final_comp.lower() == 'cmb':
+        freqscale_fac = np.ones(nc)
+
+    elif final_comp.lower() == 'tsz' or final_comp.lower() == 'y':
+
+        freqscale_fac = []
+        for freq in sorted( freqarr ):
+            freqscale_fac.append( compton_y_to_delta_Tcmb(freq * 1e9) )
+
+        freqscale_fac = np.asarray( freqscale_fac )
+
+        if final_comp.lower() == 'tsz': #tsz at 150 GHz
+            freqscale_fac = freqscale_fac / freqscale_fac[1]
+
+    acap = np.zeros(nc) + (freqscale_fac * freqcalib_fac) #assuming CMB is the same and calibrations factors are same for all channel
+    acap = np.mat(acap).T #should be nc x 1
+
+    return acap
+
+################################################################################################################
+
 def get_ilc_map(final_comp, map_dic, bl_dic, nside, lmax, cl_dic = None, nl_dic = None, lmin = 10, freqcalib_fac = None, ignore_fg = [], full_sky = 0, mapparams = None, apod_mask = None):
 
     """
@@ -174,117 +316,6 @@ def get_multipole_weightsarr(final_comp, freqarr, el, cl_dic, lmin, freqcalib_fa
             weightsarr[:, elcnt] = weight_mat
 
     return weightsarr
-
-################################################################################################################
-
-def get_covariance_dic(param_dict, freqarr, nl_dic = None, ignore_fg = []):
-
-    #get the Cls for the baseline band
-    el, cl_cmb = fg.get_foreground_power_george_2015('CMB', freq1 = param_dict['freq0'], freq2 = param_dict['freq0'])
-    el, cl_ksz = fg.get_foreground_power_george_2015('kSZ', freq1 = param_dict['freq0'], freq2 = param_dict['freq0'])
-
-    cl_dic = {}
-
-    for freq1 in freqarr:
-        for freq2 in freqarr:
-
-            if (freq2, freq1) in cl_dic:
-                cl_dic[(freq1, freq2)] = cl_dic[(freq2, freq1)]
-                continue
-
-            #get dust
-            el,  cl_dg_po, cl_dg_clus = fg.get_cl_dust(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'], spec_index_dg_po = param_dict['spec_index_dg_po'], spec_index_dg_clus = param_dict['spec_index_dg_clus'], Tcib = param_dict['Tcib'])
-
-            #get tsz
-            el, cl_tsz = fg.get_cl_tsz(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'])
-
-            #get radio
-            el, cl_radio = fg.get_cl_radio(freq1, freq2, freq0 = param_dict['freq0'], fg_model = param_dict['fg_model'], spec_index_rg = param_dict['spec_index_rg'])
-
-            if 'cmb' not in ignore_fg:
-                cl = np.copy(cl_cmb)
-            if 'ksz' not in ignore_fg:
-                cl = cl + cl_ksz             
-            if 'tsz' not in ignore_fg:
-                cl = cl + cl_tsz
-            if 'radio' not in ignore_fg:
-                cl = cl + cl_radio
-            if 'dust' not in ignore_fg:
-                cl = cl + cl_dg_po + cl_dg_clus
-
-            #make sure cl start from el=0 rather than el=10 which is the default for SPT G15 results
-            lmin = min(el)
-            cl = np.concatenate( (np.zeros(lmin), cl) )
-
-            #noise auto power spectrum
-            if nl_dic is not None:
-
-                nl = nl_dic[freq1]                    
-                if freq1 != freq2: 
-                    nl = np.copy(nl) * 0.
-
-                if len(cl) > len(nl):
-                    cl = cl[:len(nl)]
-                elif len(cl) < len(nl):
-                    nl = nl[:len(cl)]
-
-                el = np.arange(len(cl))
-
-            else:
-                nl = np.zeros(len(cl))
-                #20191116 - fix this: there must be noise correlation in case of atmospheric noise
-                print('\n\n\t\tfix me: there must be noise correlation in case of atmospheric noise')
-
-            cl = cl + nl
-
-            ##loglog(cl); title('%s - %s' %(freq1, freq2)); show()
-
-            cl_dic[(freq1, freq2)] = cl 
-
-    return el, cl_dic  
-
-################################################################################################################
-################################################################################################################
-
-def create_clmat(freqarr, elcnt, cl_dic):
-    """
-    freqarr  = array of frequency channel
-    elcnt = \el index
-    cl_dic = cl_cmb + cl_FG auto and cross spectra fr thefrequency channel
-    """
-    nc = len(freqarr)
-    clmat = np.zeros( (nc, nc) )
-    for ncnt1, freq1 in enumerate(freqarr):
-        for ncnt2, freq2 in enumerate(freqarr):
-            clmat[ncnt2, ncnt1] = cl_dic[(freq1, freq2)][elcnt]
-    return clmat
-
-################################################################################################################
-
-def get_acap(freqarr, final_comp = 'CMB', freqcalib_fac = None):
-
-    nc = len(freqarr)
-
-    if freqcalib_fac is None: freqcalib_fac = np.ones(nc)
-
-    if final_comp.lower() == 'cmb':
-        freqscale_fac = np.ones(nc)
-
-    elif final_comp.lower() == 'tsz' or final_comp.lower() == 'y':
-
-        freqscale_fac = []
-        for freq in sorted( freqarr ):
-            freqscale_fac.append( compton_y_to_delta_Tcmb(freq * 1e9) )
-
-        freqscale_fac = np.asarray( freqscale_fac )
-
-        if final_comp.lower() == 'tsz': #tsz at 150 GHz
-            freqscale_fac = freqscale_fac / freqscale_fac[1]
-
-    acap = np.zeros(nc) + (freqscale_fac * freqcalib_fac) #assuming CMB is the same and calibrations factors are same for all channel
-    acap = np.mat(acap).T #should be nc x 1
-
-    return acap
 
 ################################################################################################################
 
